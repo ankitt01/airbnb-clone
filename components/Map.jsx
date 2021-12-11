b1c4ff0b07daa8ed44a2cb33814665598a50f932
ankitt01 <ankitrawat0102@gmail.com>
2021-12-11 22:09:32 +0530

    geolib getcenter

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
-import ReactMapGL from 'react-map-gl';
+import React, {useState} from 'react'
+import ReactMapGL, {Marker, Popup} from 'react-map-gl';
+// import getCenter from 'geolib/es/getCenter'
+import * as geolib from 'geolib';
 
 
-function Map() {
+function Map({searchResults}) {
+
+    
+
+
+    //Transform the search result object into the lang lat object
+
+    const coordinates = searchResults.map((result) => (
+        {
+            longitude: result.long,
+            latitude: result.lat,
+        }
+    ))
+    const center = geolib.getCenter(coordinates)
+
+    const [viewport,setViewport] = useState({
+        width: '100%',
+        height: '100%',
+        latitude: center.latitude,
+        longitude: center.longitude,
+        zoom:11,
+    });
+
     return (
         <ReactMapGL 
-                
+            mapStyle="mapbox://styles/ankit0102/ckwz9tx1m0tnd15qad6h26oks"
+            mapboxApiAccessToken={process.env.mapbox_key}
+            {...viewport}
+            onViewportChange={(nextViewport) => setViewport(nextViewport)}
         >
 
         </ReactMapGL>
--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     images: {
-        domains: ['links.papareact.com']
+        domains: ['links.papareact.com'],
+    },
+    env: {
+        mapbox_key: "pk.eyJ1IjoiYW5raXQwMTAyIiwiYSI6ImNrd3o5anJndDB2MHoydXVycjFoemtndWIifQ.WA4Nw9EN5xFsyw_VzumYBg"
     }
 }
